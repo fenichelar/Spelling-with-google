d60053b4672029e846109d6a1785d7ffbf76ecaa
Piotr Wilczynski <bevesce@gmail.com>
2013-05-08 16:47:19 +0200

    now uses google api

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Workflow looks up spelling of the word on the google search results page and pastes it into current application.
+Workflow/script looks up spelling of the word on the google search results page and pastes it into current application/puts result to clipboard.
+
+You need to provide at least one google api key that can be obtained on https://code.google.com/apis/console/ (with active custom search service).
--- google_spelling.py
+++ google_spelling.py
@@ -1,27 +1,47 @@
 import urllib2
-from cgi import escape
+from urllib import urlencode
+import json
+
 
 url = "http://www.google.com/search?q={query}"
 
 hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'}
+query = """https://www.googleapis.com/customsearch/v1?num=1&"""
+
+keys = [
+    # provide at least one google api key from https://code.google.com/apis/console/
+]
+
+'key={key}&cx={cx}&num=1&q={q}'
+
+pl_cse = '009341357436794688484:bi6ngbl4mvk'
+en_cse = '009341357436794688484:rvhujssywbq'
 
 
 def correct(word):
     result = word
-    try:
-        req = urllib2.Request(
-            url.format(query=escape(word)),
-            headers=hdr
-        )
-        html = urllib2.urlopen(req).read()
-        result = html.split('<a class="spell"')[1].split("<i>")[1].split("</i>")[0]
-    except:
-        pass
-    return result
+    for key in keys:
+        try:
+            full_query = query + (
+                urlencode({'key': key, 'cx': pl_cse, 'q': word})
+            )
+
+            search_result = json.loads(
+                urllib2.urlopen(full_query).read()
+            )
+            if 'spelling' in search_result:
+                return search_result['spelling']['correctedQuery']
+            else:
+                return result
+        except urllib2.HTTPError:
+            continue
+    else:
+        return result + '.no_key'
+
 
 if __name__ == "__main__":
     import sys
     import subprocess
-    c = correct(" ".join(sys.argv[1:])).strip()
+    c = correct(" ".join(sys.argv[1:])).strip().encode('utf-8')
     sys.stdout.write(c)
     subprocess.call("""echo "{0}" | tr -d '\n' | pbcopy""".format(c), shell=True)
